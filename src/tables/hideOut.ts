d95ca3e714ee3492c68c61be93ea98505d506a9c
Kojanzy <nwmarinoto@gmail.com>
2023-01-03 14:40:39 -0500

    hideout integration and according modifications

---
+++ src/tables/hideOut.ts
@@ -0,0 +1,52 @@
+import * as fs from "fs";
+import toml from "toml";
+import { container } from "tsyringe";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+
+const config = toml.parse(fs.readFileSync("./../config.toml","utf-8"));
+
+function constructionModify(configCase: number): void
+{
+    // table and hideout area declaration
+    const tables = container.resolve<DatabaseServer>("DatabaseServer").getTables();
+    const hideoutConstruction = tables.hideout.areas;
+
+    // iterating through every area
+    for (const area in hideoutConstruction)
+    {   // iterating through every stage
+        for (const stage in hideoutConstruction[area].stages)
+        { 
+            const current = hideoutConstruction[area].stages[stage];
+            if (configCase === 1) // if the user wants to make construction instant...
+                current.constructionTime = 1;
+            else if (configCase === 2) // if the user wants to modify construction time...
+                current.constructionTime *= config["Construction Modifier"];
+        }
+    } 
+}
+
+function productionModify(configCase: number): void
+{
+    // table and hideout production declaration
+    const tables = container.resolve<DatabaseServer>("DatabaseServer").getTables();
+    const hideoutProduction = tables.hideout.production;
+
+    // iterating through all items up for production
+    for (const id in hideoutProduction)
+    {   
+        const current = hideoutProduction[id];
+        if (configCase === 1) // if the user wants to make production instant...
+            current.productionTime = 1;
+        else if (configCase === 2) // if the user wants to modify production time...
+            current.productionTime *= config["Production Modifier"];
+    }
+}
+
+export default function init(): void
+{
+    // function calls in config decision cases
+    if (config["Instant Construction"]) constructionModify(1);
+    if (config["Construction Modifier"] !== 1.0) constructionModify(2);
+    if (config["Instant Production"]) productionModify(1);
+    if (config["Production Modifier"] !== 1.0) productionModify(2);
+}
--- src/tables/index.ts
+++ src/tables/index.ts
@@ -2,6 +2,7 @@ import { default as initPlayer } from "./player";
 import { default as initSkills } from "./skills";
 import { default as initBots } from "./bots";
 import { default as initTraders } from "./traders";
+import { default as initHideOut } from "./hideOut";
 
 export function initMod(): boolean
 {
@@ -11,6 +12,7 @@ export function initMod(): boolean
         initSkills();
         initBots();
         initTraders();
+        initHideOut();
         return true;
     }
     catch (err)
