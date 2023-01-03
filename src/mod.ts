b623816d02a5ece2974a059e3b961dbe234e7d7a
Kojanzy <nwmarinoto@gmail.com>
2023-01-03 13:13:32 -0500

    throw init into index and mod cleanup

--- src/mod.ts
+++ src/mod.ts
@@ -1,30 +1,15 @@
 import * as fs from "fs";
-import * as tables from "./tables/index";
 import { DependencyContainer } from "tsyringe";
 import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+import { initMod } from "./tables/index";
 
 export class Mod implements IPostDBLoadMod
 {
     public postDBLoad(container: DependencyContainer): void
     {
         const logger = container.resolve<ILogger>("Logger");
-        try
-        {
-            tables.initPlayer();
-            tables.initSkills();
-            tables.initBots();
-        }
-        catch (err)
-        {
-            logger.info(err);
-        }
-        finally
-        { 
-            fs.readdir("./tables/", (err, files) =>
-            {
-                logger.info(`Initialized ${files.length - 1} SPT-VM modules.`);
-            });
-        }
+        initMod();
+        fs.readdir("./tables/", (err, files) => logger.info(`Initialized ${files.length - 1} SPT-VM modules.`));
     }
 }
--- src/tables/index.ts
+++ src/tables/index.ts
@@ -1,3 +1,18 @@
-export { default as initPlayer } from "./player";
-export { default as initSkills } from "./skills";
-export { default as initBots } from "./bots";
+import { default as initPlayer } from "./player";
+import { default as initSkills } from "./skills";
+import { default as initBots } from "./bots";
+
+export function initMod(): boolean
+{
+    try
+    {
+        initPlayer();
+        initSkills();
+        initBots();
+        return true;
+    }
+    catch (err)
+    {
+        return false;
+    }
+}
