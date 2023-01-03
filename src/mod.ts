57348a1afd284c74ea8aca93fcf23fc50c6d7dd8
Kojanzy <nwmarinoto@gmail.com>
2023-01-03 11:35:05 -0500

    switch to postDBLoad

--- src/mod.ts
+++ src/mod.ts
@@ -1,18 +1,18 @@
 import * as fs from "node:fs";
 import * as tables from "./tables/index";
 import { DependencyContainer } from "tsyringe";
-import { IPostAkiLoadMod } from "@spt-aki/models/external/IPostAkiLoadMod";
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
-//import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 
-export class Mod implements IPostAkiLoadMod
+export class Mod implements IPostDBLoadMod
 {
-    public postAkiLoad(container: DependencyContainer): void
+    public postDBLoad(container: DependencyContainer): void
     {
         tables.initPlayer();
 
         const logger = container.resolve<ILogger>("Logger");
-        fs.readdir("./tables/", (err, files) => {
+        fs.readdir("./tables/", (err, files) =>
+        {
             logger.info(`Initialized ${files.length - 1} tables.`);
         });
     }
